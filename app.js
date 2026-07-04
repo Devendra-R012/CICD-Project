7af94384a800bb2b7f20cdf65423ffe682de2acd
EC2 Default User <ec2-user@ip-172-31-19-38.ap-south-1.compute.internal>
2026-07-04 09:12:50 +0000

    updated app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hii welcome to CI/CD Pipeline');
+    res.send('Hello welcome to CI/CD Pipeline');
 });
 
 app.listen(PORT, () => {
